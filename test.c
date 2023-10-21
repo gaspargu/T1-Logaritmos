e7562eae2612f1158053318e0c6fc49125f492da
gaspargu <gasp.gumucio@gmail.com>
2023-10-21 18:36:07 -0300

    Mejora legibilidad en los tests

--- test.c
+++ test.c
@@ -9,11 +9,16 @@
 int main() {
     // ejemplo 1
     Rect r[6] = {{1,1,2,2}, {3,1,4,3}, {3,2,6,4}, {6,1,7,2}, {7,1,8,2}, {8,1,9,4}}; 
+    printf("Creamos un RTree en un archivo binario para la siguiente lista de rectangulos:\n");
+    printArrayOfRect(r,6);
     constructRTreeBin("archivo",r,6,2);
-    readFile("archivo");
+
+    printf("Buscamos los rectangulos que intersecten con el rectangulo ");
     Rect C = {1,1,6,3};
+    printRect(C);
     Res *res;
     res = buscar("archivo", 6, 2, C);
+    printf("Los rectangulos encontrados son: ");
     printArrayOfRect(res->array, res->n);
     return 0;
 }
--- test1.c
+++ test1.c
@@ -16,36 +16,27 @@ int main() {
     Rect r5 = {7,1,8,2};
     Rect r6 = {8,1,9,4};
     if (intersect(r2,r3) && !intersect(r1,r2)) {
-        printf("Test intersect aprobado\n\n");
+        printf("Test interseccion de rectangulos aprobado.\n\n");
     }
 
 
     // test ordena rectangulos por coordenada x
     Rect *arr = Rset(4);
     Rect *arr_sort = sortByX(arr, 4);
+    printf("Arreglo de rectangulos desordenados:\n");
     printArrayOfRect(arr,4);
+    printf("Ordenado por coordenada x:\n");
     printArrayOfRect(arr_sort,4);
+    printf("\n");
 
     // test MBR
     Rect r[] = {r1,r2,r3};
+    printf("El MBR de : ");
+    printArrayOfRect(r,3);
+    printf("es ");
     Rect mbr = MBR(r, 3);
     printRect(mbr);
 
-    // test crear RTree
-    Node *n=malloc(sizeof(Node));
-    n->num_keys = 6;
-    Rect rectangle_array[] = {r1,r2,r3,r4,r5,r6};
-    n->keys = malloc(6 * sizeof(Rect));
-    memcpy(n->keys, rectangle_array, 6 * sizeof(Rect));
-    n->childs = malloc(6 * sizeof(Node *));
-    for (int i=0; i<6; i++) {
-        n->childs[i] = NULL;
-    }
-    Node *n_rtree = createRTree(n, n->num_keys, 2, 1);
-    printRTree(n_rtree);
-
-    printf("%i",(sizeof(-1)));
-    writeBinFile("hola");
 
     return 0;
 }
